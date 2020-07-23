bb1737e33bc073def8e0bbbedbbb800545457abd
XYQ <xyq@XYQdeMBP.lan>
2020-07-23 17:46:22 +0800

    feat : complete session

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,84 +1,10 @@
 import pytest
 from tests.api.httpbin import * 
 
-
 # 第一个单元测试
 def test_version():
     from apitest import __version__
     assert isinstance(__version__,str)
-
-# import requests
-
-# class BaseApi(object):
-
-#     method = "get"
-#     url = ""
-#     params = {}
-#     headers = {}
-#     data = {}
-#     json = {}
-
-#     def set_params(self,**params):
-#         self.params = {}
-#         return self
-    
-#     def set_data(self,data):
-#         self.data = data
-#         return self
-
-#     def set_json(self,json_data):
-#         self.json = json_data
-#         return self
-
-#     def run(self):
-#         self.responce = requests.request(self.method,self.url,params=self.params,headers=self.headers,data=self.data,json=self.json)
-#         return self
-
-#     def validate(self,key,value):
-#         actual_value = getattr(self.responce,key)
-#         assert actual_value == value
-#         return self   
-    
-
-# class ApiHttpbinGet(BaseApi):
-    
-#     url = "https://httpbin.org/get"
-#     params = {}
-#     method = "GET"
-#     headers = {"Accept": "application/json"}
-#     data = ""
-#     json = {}
-
-    # def set_params(self,**params):
-    #     self.params = {}
-    #     return self
-
-    # def run(self):
-    #     self.responce = requests.request(self.method,self.url,params=self.params,headers=self.headers,data=self.data,json=self.json)
-    #     return self
-
-    # def validate(self,key,value):
-    #     actual_value = getattr(self.responce,key)
-    #     assert actual_value == value
-    #     return self
-
-# class ApiHttpbinPost(BaseApi):
-    
-#     url = "https://httpbin.org/post"
-#     method = "POST"
-#     headers = {"Accept": "application/json"}
-#     json = {"xyq":18}
-
-    # def run(self):
-    #     self.responce = requests.request(self.method,self.url,headers=self.headers,json=self.json)
-    #     return self   
-
-    # def validate(self,key,value):
-    #     actual_value = getattr(self.responce,key)
-    #     assert actual_value == value
-    #     return self
-
-
 
 def test_httpbin_get():
     # resp = requests.get("https://httpbin.org/get")
@@ -201,3 +127,77 @@ def test_httpbin_login_status(init_session):
         .get_responce()
     request_headers = resp.request.headers
     assert "freeform=567" in request_headers["Cookie"]
+
+# import requests
+
+# class BaseApi(object):
+
+#     method = "get"
+#     url = ""
+#     params = {}
+#     headers = {}
+#     data = {}
+#     json = {}
+
+#     def set_params(self,**params):
+#         self.params = {}
+#         return self
+    
+#     def set_data(self,data):
+#         self.data = data
+#         return self
+
+#     def set_json(self,json_data):
+#         self.json = json_data
+#         return self
+
+#     def run(self):
+#         self.responce = requests.request(self.method,self.url,params=self.params,headers=self.headers,data=self.data,json=self.json)
+#         return self
+
+#     def validate(self,key,value):
+#         actual_value = getattr(self.responce,key)
+#         assert actual_value == value
+#         return self   
+    
+
+# class ApiHttpbinGet(BaseApi):
+    
+#     url = "https://httpbin.org/get"
+#     params = {}
+#     method = "GET"
+#     headers = {"Accept": "application/json"}
+#     data = ""
+#     json = {}
+
+    # def set_params(self,**params):
+    #     self.params = {}
+    #     return self
+
+    # def run(self):
+    #     self.responce = requests.request(self.method,self.url,params=self.params,headers=self.headers,data=self.data,json=self.json)
+    #     return self
+
+    # def validate(self,key,value):
+    #     actual_value = getattr(self.responce,key)
+    #     assert actual_value == value
+    #     return self
+
+# class ApiHttpbinPost(BaseApi):
+    
+#     url = "https://httpbin.org/post"
+#     method = "POST"
+#     headers = {"Accept": "application/json"}
+#     json = {"xyq":18}
+
+    # def run(self):
+    #     self.responce = requests.request(self.method,self.url,headers=self.headers,json=self.json)
+    #     return self   
+
+    # def validate(self,key,value):
+    #     actual_value = getattr(self.responce,key)
+    #     assert actual_value == value
+    #     return self
+
+
+
